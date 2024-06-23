5dd6be46463302e4fee2e77d75e3c10673e6059d
AntonioFeitosaAm <acgfnubank@gmail.com>
2024-06-23 18:41:44 -0400

    Exceção personalizada, com entrada de dados pelo terminal.

--- ControleFluxo/src/Contador.java
+++ ControleFluxo/src/Contador.java
@@ -5,23 +5,40 @@ public class Contador {
     public static void main(String[] args) {
 		Scanner terminal = new Scanner(System.in);
 		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.??;
+		//int parametroUm = 3;
+		int parametroUm = terminal.nextInt();
 		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.??;
+		//int parametroDois = 10;
+		int parametroDois = terminal.nextInt();
 		
 		try {
 			//chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
 		
-		}catch (? exception) {
+		}catch (ParametrosInvalidosException e) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
+			
 		}
-		
-	}
+    }
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if (parametroUm > parametroDois){
+			throw new ParametrosInvalidosException();	
+		}else{
+			//realizar o for para imprimir os números com base na variável contagem
+			if (parametroUm < parametroDois){
+				int contagem = parametroDois - parametroUm;
+				for(int x=1; x <= contagem;x++){
+					System.out.println("Numero: " + x);
+				}
+			}
+			
+		}
 		
-		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
+		
+		
 	}
-}
+		
+} 
+
